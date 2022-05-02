9787090c218c3b7c937aa17d4bcfae4bf2c8d233
Shota Miyahira <2525.natuta@gmail.com>
2022-05-02 11:39:07 +0900

    tailwind対応

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import {TailwindProvider} from 'tailwind-rn';
+
 import { MainRoot } from './src/roots/MainRoot';
+import utilities from './tailwind.json';
+
 
 export default function App() {
   return (
-    <MainRoot />
+    <TailwindProvider utilities={utilities}>
+      <MainRoot />
+    </TailwindProvider>
   );
 }
